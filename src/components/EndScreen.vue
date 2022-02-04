7c7a2703321fb3e494d8b099245dbb4996083167
birdsovparadise <davidsabel86@gmail.com>
2022-02-04 07:57:51 +0100

    oi!

--- src/components/EndScreen.vue
+++ src/components/EndScreen.vue
@@ -1,42 +1,40 @@
 <template>
-<<<<<<< HEAD
-  <div>
-    <table class="table">
-      <thead>
-        <tr>
-          <th scope="col">Köp</th>
-          <th scope="col">Varor</th>
-          <th scope="col">Antal</th>
-          <th scope="col">Pris</th>
-        </tr>
-      </thead>
-      <tbody>
-        <tr>
-          <th scope="row">1</th>
-          <td>Kattavel</td>
-          <td>6</td>
-          <td>6000 kr</td>
-        </tr>
-        <tr>
-          <th scope="row">2</th>
-          <td>Hö och grismat</td>
-          <td>1000</td>
-          <td>10 000 kr</td>
-        </tr>
-        <tr>
-          <th scope="row">3</th>
-          <td>Larry</td>
-          <td>the Bird</td>
-          <td>@twitter</td>
-        </tr>
-      </tbody>
-    </table>
-  </div>
-=======
+	<div>
+		<table class="table">
+			<thead>
+				<tr>
+					<th scope="col">Köp</th>
+					<th scope="col">Varor</th>
+					<th scope="col">Antal</th>
+					<th scope="col">Pris</th>
+				</tr>
+			</thead>
+			<tbody>
+				<tr>
+					<th scope="row">1</th>
+					<td>Kattavel</td>
+					<td>6</td>
+					<td>6000 kr</td>
+				</tr>
+				<tr>
+					<th scope="row">2</th>
+					<td>Hö och grismat</td>
+					<td>1000</td>
+					<td>10 000 kr</td>
+				</tr>
+				<tr>
+					<th scope="row">3</th>
+					<td>Larry</td>
+					<td>the Bird</td>
+					<td>@twitter</td>
+				</tr>
+			</tbody>
+		</table>
+	</div>
+	=======
 	<div>
 		<p>Hej</p>
 	</div>
->>>>>>> 3740811abbb79c1504b3f7dc9e677e8f9b503bd0
 </template>
 
 <script>
