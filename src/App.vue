b74d4117cf04167566c2e5df9e7d6e8a0a22aa12
Linus Romland <hello@linusromland.com>
2022-02-04 08:54:24 +0100

    Moved Nav Elements to separate component and some comments

--- src/App.vue
+++ src/App.vue
@@ -1,21 +1,7 @@
 <template>
-	<nav>
-		<ul>
-			<li>
-				<RouterLink to="/">Hem</RouterLink>
-			</li>
-			<li>
-				<RouterLink to="/about">Om</RouterLink>
-			</li>
-			<li>
-				<RouterLink to="/contact">Kontakt</RouterLink>
-			</li>
+	<!-- Navbar  -->
+	<Navbar />
 
-			<li>
-				<RouterLink to="/djur">Djur-sidan...</RouterLink>
-			</li>
-		</ul>
-	</nav>
 	Här nedan kommer main och RouterView:
 	<main>
 		<RouterView />
@@ -26,11 +12,15 @@
 </template>
 
 <script>
+	//Component Imports:
 	import EndScreen from './components/EndScreen.vue';
+	import Navbar from './components/Navbar.vue';
 
 	export default {
+		name: 'App',
 		components: {
-			EndScreen
+			EndScreen,
+			Navbar
 		}
 	};
 </script>
---
+++ src/components/Navbar.vue
@@ -0,0 +1,25 @@
+<template>
+	<nav>
+		<ul>
+			<li>
+				<RouterLink to="/">Hem</RouterLink>
+			</li>
+			<li>
+				<RouterLink to="/about">Om</RouterLink>
+			</li>
+			<li>
+				<RouterLink to="/contact">Kontakt</RouterLink>
+			</li>
+
+			<li>
+				<RouterLink to="/djur">Djur-sidan...</RouterLink>
+			</li>
+		</ul>
+	</nav>
+</template>
+
+<script>
+	export default {
+		name: 'Navbar'
+	};
+</script>
