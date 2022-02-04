e8a58e78c230094f25ea454c0613f4a21b31cb71
birdsovparadise <davidsabel86@gmail.com>
2022-02-04 07:53:39 +0100

    Update EndScreen.vue

--- src/components/EndScreen.vue
+++ src/components/EndScreen.vue
@@ -2,6 +2,35 @@
 
 <template>
   <div>
-    <p>Hej</p>
+    <table class="table">
+      <thead>
+        <tr>
+          <th scope="col">Köp</th>
+          <th scope="col">Varor</th>
+          <th scope="col">Antal</th>
+          <th scope="col">Pris</th>
+        </tr>
+      </thead>
+      <tbody>
+        <tr>
+          <th scope="row">1</th>
+          <td>Kattavel</td>
+          <td>6</td>
+          <td>6000 kr</td>
+        </tr>
+        <tr>
+          <th scope="row">2</th>
+          <td>Hö och grismat</td>
+          <td>1000</td>
+          <td>10 000 kr</td>
+        </tr>
+        <tr>
+          <th scope="row">3</th>
+          <td>Larry</td>
+          <td>the Bird</td>
+          <td>@twitter</td>
+        </tr>
+      </tbody>
+    </table>
   </div>
 </template>
