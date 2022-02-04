e6e6cf8b9320a90af0ec9c37d7bbb51cb34b2d20
Fredric Ryding <fredricr91@hotmail.com>
2022-02-04 13:21:23 +0100

    Added button to cart and some changes in CartView.vue

--- src/views/CartView.vue
+++ src/views/CartView.vue
@@ -1,3 +1,16 @@
+<style>
+	h1 {
+		margin-bottom: 20px;
+	}
+
+	#checkOut {
+		width: 100%;
+	}
+</style>
 <template>
-	<p>Min varukorg</p>
+	<h1>Min varukorg</h1>
+	<p>Added item*</p>
+	<p>Added item*</p>
+	<p>Added item*</p>
+	<button id="checkOut" type="button" class="btn btn-secondary">Gå vidare till kassan</button>
 </template>
