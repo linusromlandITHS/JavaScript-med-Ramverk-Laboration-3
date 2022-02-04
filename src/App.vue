3c51f7861738bc1634a65fb3f2fcac90b519a461
Linus Romland <hello@linusromland.com>
2022-02-04 13:05:31 +0100

    Fixed lint error and converted text to comments

--- src/App.vue
+++ src/App.vue
@@ -1,21 +1,16 @@
 <template>
-
-
 	<!-- Navbar  -->
 	<Navbar />
 
->>>>>>> 3428365c4e419a428ce435092829e3f9d0ba1f10
-	Här nedan kommer main och RouterView:
-	<main style="padding:35px; border:1px solid">
+	<!-- Här nedan kommer main och RouterView: -->
+	<main style="padding: 35px; border: 1px solid">
 		<RouterView />
 	</main>
-  <hr />
-Detta nedanför kanske inte ska ligga kvar?
-	Main slut
+	<hr />
+	<!-- Detta nedanför kanske inte ska ligga kvar?
+	Main slut -->
 
-  
 	<EndScreen />
-</div>
 </template>
 
 <script>
