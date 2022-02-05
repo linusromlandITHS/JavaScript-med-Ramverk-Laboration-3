6073391c62232da7675b0ed888bd0029320b9408
Linus Romland <romlandlinus@gmail.com>
2022-02-05 22:10:27 +0100

    Started on Product View

---
+++ src/views/ProductView.vue
@@ -0,0 +1,49 @@
+<template>
+	<!-- This is a very very temporary showing of data!!! -->
+	<h1>{{ product.name }}</h1>
+	<img v-for="(image, index) in images" :key="index" :src="image" />
+</template>
+
+<script>
+	export default {
+		name: 'ProductView',
+		data() {
+			return {
+				product: {} //Object containg all data for product
+			};
+		},
+		methods: {
+			/**
+			 * @author Linus Romland (hello@linusromland.com)
+			 *
+			 * @name fetchProduct
+			 * @description Fetches product data from database.json
+			 * @param {string} id - The id of the product to fetch
+			 * @returns {Promise} - A promise that resolves to the product data
+			 */
+			async fetchProduct() {
+				//Fetch product data from database
+				const request = await fetch(`/database.json`);
+				const response = await request.json();
+
+				//If failed request, redirect to home page
+				if (!response) this.$router.push('/');
+
+				//Finds the product that matches the param id
+				this.product = response.animals.find((product) => product.id === this.$route.params.id);
+
+				//If product is not found, redirect to home page
+				if (!this.product) this.$router.push('/');
+				console.log(this.product);
+			}
+		},
+		computed: {
+			images() {
+				return this.product.images.map((image) => `../../assets/products/${image}`);
+			}
+		},
+		created() {
+			this.fetchProduct();
+		}
+	};
+</script>
