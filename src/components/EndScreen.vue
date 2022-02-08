186a88fdc895e93d03eb6f1a3f5b0d88460eceb0
birdsovparadise <davidsabel86@gmail.com>
2022-02-08 11:49:22 +0100

    Update EndScreen.vue

--- src/components/EndScreen.vue
+++ src/components/EndScreen.vue
@@ -78,18 +78,62 @@
 				</tr>
 			</tbody>
 		</table>
-		<button class="btn-primary">Betala</button>
+		<div class="container" id="table">
+			<div class="card px-4">
+				<p class="h8 py-3">Betalnings information:</p>
+				<div class="row gx-3">
+					<div class="col-12">
+						<div class="d-flex flex-column">
+							<p class="text mb-1">Namn och efternamn</p>
+							<input class="form-control mb-3" type="text" placeholder="Name" value="Barry Allen" />
+						</div>
+					</div>
+					<div class="col-12">
+						<div class="d-flex flex-column">
+							<p class="text mb-1">Kort nummer</p>
+							<input class="form-control mb-3" type="text" placeholder="1234 5678 435678" />
+						</div>
+					</div>
+					<div class="col-6">
+						<div class="d-flex flex-column">
+							<p class="text mb-1">Utgångsdatum</p>
+							<input class="form-control mb-3" type="text" placeholder="MM/YYYY" />
+						</div>
+					</div>
+					<div class="col-6">
+						<div class="d-flex flex-column">
+							<p class="text mb-1">CVV/CVC</p>
+							<input class="form-control mb-3 pt-2" type="password" placeholder="***" />
+						</div>
+					</div>
+					<div class="col-12">
+						<div class="btn btn-primary mb-3">
+							<span class="ps-3">Betala 16400 kr</span> <span class="fas fa-arrow-right" />
+						</div>
+					</div>
+				</div>
+			</div>
+		</div>
 	</div>
 </template>
 
 <style>
-	table {
+	.table {
 		padding: 30px;
 		background-color: #c9c9bd;
 		border: 4px;
 		border-color: #faac77;
 		box-shadow: 0 4px 8px 0 rgba(0, 0, 0, 0.2), 0 6px 20px 0 rgba(0, 0, 0, 0.19);
 	}
+	#table {
+		color: black;
+		padding: 30px;
+		background-color: #c9c9bd;
+		border: 4px;
+		border-color: #faac77;
+		box-shadow: 0 4px 8px 0 rgba(0, 0, 0, 0.2), 0 6px 20px 0 rgba(0, 0, 0, 0.19);
+	}
+
 	div {
 		border: 4px;
 		border-color: #faac77;
@@ -107,6 +151,6 @@
 		cursor: pointer;
 	}
 	span:hover {
-		color: white;
+		color: black;
 	}
 </style>
