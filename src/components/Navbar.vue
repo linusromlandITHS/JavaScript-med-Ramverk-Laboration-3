cd4d3d1e44b44a4e0ed45aae9fe7feff3097c651
kennethwi <23746270+kennethwi@users.noreply.github.com>
2022-02-14 10:40:39 +0100

    Still changing testpetlisting to pet listing. Overwritten?

--- src/components/Navbar.vue
+++ src/components/Navbar.vue
@@ -29,7 +29,7 @@
 					<li><RouterLink to="/">Hem</RouterLink></li>
 					<li><RouterLink to="/contact">Kontakt</RouterLink></li>
 					<li><RouterLink to="/djur">Sida för att se innehållet i DB...</RouterLink></li>
-					<li><RouterLink to="/testpetlisting">Temp. version av listningssidan</RouterLink></li>
+					<li><RouterLink to="/petlisting">Temp. version av listningssidan</RouterLink></li>
 				</div>
 			</div>
 		</div>
