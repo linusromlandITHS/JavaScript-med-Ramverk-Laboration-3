b35c269d25e865e635e000ff70b6375ba1e50f0b
Fredric Ryding <fredricr91@hotmail.com>
2022-03-02 12:18:06 +0100

    Small fixes

--- src/App.vue
+++ src/App.vue
@@ -41,22 +41,14 @@
 </script>
 
 <template>
-	<div
-		id="topdiv"
-		style="backdrop-filter: blur(16px) saturate(180%); -webkit-backdrop-filter: blur(16px) saturate(180%)"
-	>
-		<!-- height: 70vh;
-			background-color: #e7f9fb;
-			position: relative; -->
-		<!-- Navbar  -->
-		<Navbar />
-		<DiscountBanner />
-		<router-view v-slot="{ Component }">
-			<transition name="fade">
-				<component :is="Component" style="background: white" />
-			</transition>
-		</router-view>
-	</div>
+	<Navbar />
+	<DiscountBanner />
+	<router-view v-slot="{ Component }">
+		<transition name="fade">
+			<component :is="Component" style="background: white" />
+		</transition>
+	</router-view>
+
 	<Toast :title="toast.title" :message="toast.message" :type="toast.type" :position="toast.position" ref="toastRef" />
 
 	<backToTop />
@@ -100,22 +92,4 @@
 	.fade-leave-to {
 		opacity: 0;
 	}
-	/* #topdiv {
-		height: 40vh;
-		background-color: #73c6ef;
-		position: relative;
-	}
-
-	#topdiv::before {
-		content: '';
-		width: 100%;
-		height: 158px;
-		position: absolute;
-		bottom: -0.3%;
-		left: 0;
-		background-size: auto;
-		background-repeat: repeat no-repeat;
-		background-position: 47vw bottom;
-		background-image: url("data:image/svg+xml;utf8,<svg viewBox='0 0 1200  134' fill='none' xmlns='http://www.w3.org/2000/svg'><path d='M0 98L50 92C100 86 200 74 300 50C400 26 500 -10 600 2C700 14 800 74 900 98C1000 122 1100 110 1150 104L1200 98V134H1150C1100 134 1000 134 900 134C800 134 700 134 600 134C500 134 400 134 300 134C200 134 100 134 50 134H0V98Z' fill='%23209031'/></svg>");
-	} */
 </style>
