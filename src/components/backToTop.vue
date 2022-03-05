fea8720c49d32b362f854415369c170c931f5513
PatrykSroka21 <patryk.sroka01@gmail.com>
2022-03-05 15:53:24 +0100

    quick fix

--- src/components/backToTop.vue
+++ src/components/backToTop.vue
@@ -19,7 +19,21 @@
 </script>
 <template>
 	<div class="scroll-to-top">
-		<button @click="scrollToTop">tillbaka till toppen</button>
+		<button @click="scrollToTop">
+			<svg
+				xmlns="http://www.w3.org/2000/svg"
+				width="150"
+				height="50"
+				fill="currentColor"
+				class="bi bi-arrow-up"
+				viewBox="0 0 16 16"
+			>
+				<path
+					fill-rule="evenodd"
+					d="M8 15a.5.5 0 0 0 .5-.5V2.707l3.146 3.147a.5.5 0 0 0 .708-.708l-4-4a.5.5 0 0 0-.708 0l-4 4a.5.5 0 1 0 .708.708L7.5 2.707V14.5a.5.5 0 0 0 .5.5z"
+				/>
+			</svg>
+		</button>
 	</div>
 </template>
 <style>
@@ -27,10 +41,8 @@
 		padding: 30px 0;
 		position: absolute;
 		right: 0;
-		border-radius: 50%;
-		background-color: #faac77;
+		border-radius: 8px;
 		margin-right: 10px;
-		border: solid #606b6f 0.5px;
 	}
 	.scroll-to-top button {
 		border: none;
